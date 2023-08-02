8d7195bb3f45b449dfdfaf26cd8f442e4fdbc423
dsoto <dsoto6155@gmail.com>
2023-08-01 23:25:48 -0400

    as

--- codigo/move.sh
+++ codigo/move.sh
@@ -1,6 +1,6 @@
-chown -R apache:apache /etc/Proyectos/Regialu
-cd /etc/Proyectos/Regialu/codigo
-mv .env.example  /etc/Proyectos/Regialu/codigo/.env
+#chown -R apache:apache /var/Proyectos/Regialu
+cd /var/Proyectos/Regialu/codigo
+mv .env.example  /var/Proyectos/Regialu/codigo/.env
 chmod -R 755 ./storage
 mv ./public /var/www/regialusit/
 rm -rf ./public 
