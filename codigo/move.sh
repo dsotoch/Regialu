c3d4b038a82bed48496dc90c20b790b7106b606d
dsoto <dsoto6155@gmail.com>
2023-08-01 23:02:43 -0400

    env

--- codigo/move.sh
+++ codigo/move.sh
@@ -1,5 +1,6 @@
 chown -R apache:apache /etc/Proyectos/Regialu
 cd /etc/Proyectos/Regialu/codigo
+mv .env.example  /etc/Proyectos/Regialu/codigo/.env
 chmod -R 755 ./storage
 mv ./public /var/www/regialusit/
 rm -rf ./public 
