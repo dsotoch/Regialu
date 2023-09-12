a5aa02e1781640b7c848ba4e133b96a6941ae5ff
dsotoch <126362488+dsotoch@users.noreply.github.com>
2023-09-12 17:55:33 -0500

    Update move.sh

--- codigo/move.sh
+++ codigo/move.sh
@@ -4,5 +4,5 @@ mv .env.example  /var/www/Regialu/codigo/.env
 chmod -R 755 ./storage
 mv ./public /var/www/regialusit/
 rm -rf ./public 
-chomd -R 755  /var/www/regialusit/public
+chmod -R 755  /var/www/regialusit/public
 
